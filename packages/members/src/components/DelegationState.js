59c7b07231d8cb126aee08eb063662183b8a2849
Friedger Müffke <mail@friedger.de>
2021-05-27 15:39:25 +0200

    fix membership status

--- packages/members/src/components/DelegationState.js
+++ packages/members/src/components/DelegationState.js
@@ -2,9 +2,8 @@ import { cvToString } from '@stacks/transactions';
 import React from 'react';
 import { Amount } from './Amount';
 export function DelegationState({ delegationState }) {
-  return (
-    delegationState &&
-    (delegationState.state ? (
+  return delegationState ? (
+    delegationState.state ? (
       <>
         You have joined the pool {cvToString(delegationState.state.data['delegated-to'])} with{' '}
         <Amount ustx={delegationState.state.data['amount-ustx'].value} />.
@@ -14,6 +13,6 @@ export function DelegationState({ delegationState }) {
         You are not delegating to any pool. Join now{' '}
         <a href="https://stacks-pool-registry.pages.dev">here</a>.
       </>
-    ))
-  );
+    )
+  ) : null;
 }
--- packages/members/src/components/StackingStatus.js
+++ packages/members/src/components/StackingStatus.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { Amount } from './Amount';
 
 export function StackingStatus({ stackingStatus }) {
-  return (
-    stackingStatus &&
-    (stackingStatus.stacked ? (
+  return stackingStatus ? (
+    stackingStatus.stacked ? (
       <>
         You stacked <Amount ustx={stackingStatus.details.amount_microstx} /> until cycle #
         {stackingStatus.details.first_reward_cycle + stackingStatus.details.lock_period}.
       </>
     ) : (
       <>You are currently not stacking.</>
-    ))
-  );
+    )
+  ) : null;
 }
