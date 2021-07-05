350b738e1b99ec3fa84336c9943b9b1720e49ad4
tuffstudio <chris@tuff-studio.com>
2021-07-05 09:41:14 +0200

    Tailwind CSS

---
+++ packages/home/tailwind.config.js
@@ -0,0 +1,37 @@
+const colors = require('tailwindcss/colors')
+
+module.exports = {
+  purge: {
+    enabled: true,
+    options: {
+      keyframes: true,
+      content: ["./hugo_stats.json"],
+      defaultExtractor: (content) => {
+        let els = JSON.parse(content).htmlElements;
+        return els.tags.concat(els.classes, els.ids);
+      },
+    },
+  },
+
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      // Build your palette here
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: colors.white,
+      gray: colors.gray,
+      indigo: colors.indigo,
+    },
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif'],
+      serif: ['Nunito', 'serif'],
+    },
+    extend: {
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
