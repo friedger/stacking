4452ea6a7835b054a04f337c06d09011417844ed
friedger <mail@friedger.de>
2025-01-06 15:44:40 +0100

    add members 100, 101

--- generate.sh
+++ generate.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 cd packages/home
-counter=90
-while [ $counter -le 100 ]
+counter=101
+while [ $counter -le 110 ]
 do
   hugo new cycles/$counter.md
+  hugo new mp/cycles/$counter.md
   ((counter++))
 done
 echo All done
